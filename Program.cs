d59ecf02eea184f71039677402651b08c51fb737
soni240 <s.fegade21@gmail.com>
2022-05-18 06:54:33 +0530

    mail

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace RegexPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Pattern patterns = new Pattern();
+            //Console.WriteLine(patterns.validatePinCode("xyz@bridgelabz.com.in.in.in"));
+            if (patterns.validateString("1aaa"))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+                Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
+
+
